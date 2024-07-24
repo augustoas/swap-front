578f0d2956e4adbe25ce3230bac838f4f6a0c37c
Rodrigo Ignacio Zenteno Smith <rzenteno@mmlv.cl>
2024-07-24 17:45:21 -0400

    route to specific user table

---
+++ front/repository/userRepository.ts
@@ -0,0 +1,15 @@
+import { Resource, User } from "@/types/resource";
+
+/**
+ * Represents a repository for managing user data.
+ * @extends RepositoryFactory<User>
+ */
+export default class UserRepository extends RepositoryFactory<User> {
+  /**
+   * Creates a new instance of UserRepository.
+   * @param fetch - The fetch function used for making API requests.
+   */
+  constructor(fetch: $Fetch<User, NitroFetchRequest>) {
+    super(Resource.Users, fetch);
+  }
+}
