ae79bc49d6dd3247c86b4df5300ce9ffbbacdb93
Augusto Alvarez Smith <augustoalvarezsmith@Augustos-MacBook-Pro.local>
2024-07-28 10:16:43 -0400

    pages

---
+++ front/layouts/default.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <header>
+      <nav>
+        <ul>
+          <li>
+            <NuxtLink to="/">Home</NuxtLink>
+          </li>
+          <li>
+            <NuxtLink to="/jobs">Jobs</NuxtLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+
+    <div>
+      <slot />
+    </div>
+
+    <footer>
+      <p>&copy; 2021</p>
+    </footer>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
